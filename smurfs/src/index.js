1e5d761df2b8ceeff3ff8029fc1b104a5cf6600a
Andrea Harris <Andrea@zacs-imac.lan>
2020-01-24 12:34:02 -0600

    good on provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import {Provider} from "react-redux";
-import thunk from "redux-thunk";
-import {createStore, applyMiddleware} from "redux";
+
+
+import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import smurfReducer from "./reducers/reducer";
 
-const store =createStore(smurfReducer, applyMiddleware(thunk));
 
-ReactDOM.render <Provider store={store}><App /></Provider>, document.getElementById("root")
+const store = createStore(smurfReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+);
